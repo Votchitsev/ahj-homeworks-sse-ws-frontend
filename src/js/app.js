15e3c863064eedcbf0830a96775befdc41accc9a
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-09-17 15:17:26 +0200

    wss socket

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import Chat from './Chat';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const ws = new WebSocket('ws://shielded-springs-44422.herokuapp.com/');
+  const ws = new WebSocket('wss://shielded-springs-44422.herokuapp.com/');
   const chat = new Chat(ws);
   chat.init();
 });
